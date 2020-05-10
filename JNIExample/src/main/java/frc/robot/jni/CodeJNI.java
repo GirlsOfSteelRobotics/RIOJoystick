08ca0a0348c6295f25955ff4d764e871217da3f8
jackson <jackson@andrew.cmu.edu>
2020-05-10 18:05:23 -0400

    RobotJNIConsumer: simple test of JNILibrary sample code

--- JNIExample/src/main/java/frc/robot/jni/CodeJNI.java
+++ JNIExample/src/main/java/frc/robot/jni/CodeJNI.java
@@ -1,5 +1,11 @@
 package frc.robot.jni;
 
 public class CodeJNI {
+    // Load native library <name>.dll (Windows) or lib<name>.so (Unixes) at runtime
+    // This library contains a native method called myFunc()
+    static {
+        System.loadLibrary("JniLibrary");
+    }
+
     public static native int myFunc();
 }
