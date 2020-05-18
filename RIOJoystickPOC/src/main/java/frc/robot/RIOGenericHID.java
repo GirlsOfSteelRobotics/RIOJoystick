a57a6afd9335a757d71ec3a0d44c85fc9812d0f6
Joseph Jackson <joseph.l.jackson@gmail.com>
2020-05-17 20:59:49 -0400

    First pass at RIOGenericHID plus evdev library update

--- RIOJoystickPOC/src/main/java/frc/robot/RIOGenericHID.java
+++ RIOJoystickPOC/src/main/java/frc/robot/RIOGenericHID.java
@@ -1,15 +1,56 @@
 package frc.robot;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import com.dgis.input.evdev.devices.EvdevJoystickFilter;
+import com.dgis.input.evdev.EventDevice;
+import com.dgis.input.evdev.InputEvent;
+import com.dgis.input.evdev.InputListener;
 
 import edu.wpi.first.wpilibj.GenericHID;
 
-public abstract class RIOGenericHID extends GenericHID {
+public abstract class RIOGenericHID extends GenericHID implements InputListener {
 
-  private EvdevJoystickFilter evdev;
+  /*
+   * Event Device for the USB Joystick/gamepad device created in the constructor
+   */
+  private EventDevice m_device;
+
+  /*
+   * Holds the event codes for each joystick button, in order. That is, if event
+   * code 288 is button one, it is the first entry here.
+   */
+  private ArrayList<Integer> m_buttonEventCodes = new ArrayList<Integer>();
+
+  /*
+   * Holds the event codes for each joystick axis, in order. That is, if event
+   * code 0 is axis one, it is the first entry here.
+   */
+  private ArrayList<Integer> m_axisEventCodes = new ArrayList<Integer>();
+
+  /* Count of axes and buttons on this input device */
+  int m_axesCount;
+  int m_buttonsCount;
+
+  /*
+   * Record the state of buttons and axes as they change so we can return values
+   * whenever the robot code requests one
+   */
+  private int m_axisStates[];
+  private boolean m_buttonStates[];
+
+  /*
+   * Record any observed button presses or releases, cleared when the robot code
+   * reads them via getRawButtonPressed()
+   */
+  private boolean[] m_buttonPressed, m_buttonReleased;
+
+  /* Device number given to the constructor */
   private int m_deviceNum;
+
+  /* Rumble and LED output last set states */
   private int m_outputs;
   @SuppressWarnings("unused")
   private short m_leftRumble;
@@ -28,11 +69,99 @@ public abstract class RIOGenericHID extends GenericHID {
     super(0);
 
     m_deviceNum = deviceNum;
+    setupDevice("/dev/input/event" + deviceNum);
+  }
+
+  /*
+   * Handle one-time set up steps for opening an EventDevice by pathname
+   */
+  private void setupDevice(String devicePath) {
     try {
-      evdev = new EvdevJoystickFilter("/dev/input/event" + deviceNum);
+      m_device = new EventDevice(devicePath);
     } catch (IOException e) {
       e.printStackTrace();
+      return;
     }
+
+    Map<Integer, List<Integer>> supportedEvents = m_device.getSupportedEvents();
+    List<Integer> supportedAxes = supportedEvents.get((int) InputEvent.EV_ABS);
+    List<Integer> supportedKeys = supportedEvents.get((int) InputEvent.EV_KEY);
+
+    m_axesCount = supportedAxes == null ? 0 : supportedAxes.size();
+    m_buttonsCount = supportedKeys == null ? 0 : supportedKeys.size();
+
+    m_buttonEventCodes.addAll(supportedKeys);
+    m_axisEventCodes.addAll(supportedAxes);
+
+    System.out.println("Detected " + m_buttonEventCodes.size() + " buttons and " + m_axisEventCodes.size() + " axes.");
+
+    m_axisStates = new int[m_axesCount];
+    m_buttonStates = new boolean[m_buttonsCount];
+    m_buttonPressed = new boolean[m_buttonsCount];
+    m_buttonReleased = new boolean[m_buttonsCount];
+
+    /*
+     * Initialize the starting values of all axis to the midpoint between max and
+     * min possible values. Buttons are initialized to the unpressed state.
+     */
+    for (int i = 0; i < m_axesCount; i++)
+      m_axisStates[i] = (m_device.getAxisParameters(i).getMax() - m_device.getAxisParameters(i).getMin()) / 2;
+    for (int i = 0; i < m_buttonsCount; i++)
+      m_buttonStates[i] = m_buttonPressed[i] = m_buttonReleased[i] = false;
+
+    /* Call the event() method (below) each time an input change is detected */
+    m_device.addListener(this);
+  }
+
+  @Override
+  public void event(InputEvent e) {
+    /*
+     * If all events are null, the EvDev library was probably compiled with the
+     * wrong STRUCT_SIZE_BYTES for this architecture.
+     */
+    if (e != null) {
+      switch (e.type) {
+        case InputEvent.EV_KEY:
+          handleButton(e.code, e.value > 0);
+          break;
+        case InputEvent.EV_ABS:
+          handleAxis(e.code, e.value);
+          break;
+        case InputEvent.EV_SYN:
+          /*
+           * Indicates a set of button/axis changes detected in one pass by the driver has
+           * now been completely delivered as a series of button and/or axis events. We
+           * have no cleanup work or notifications to do, so just ignore them.
+           */
+        default:
+          /* Unknown to us, ignore */
+      }
+    }
+  }
+
+  private void handleAxis(short axisNumber, int value) {
+    int axisIndex = m_axisEventCodes.indexOf((int) axisNumber);
+    if (axisIndex < 0) {
+      System.err.println(
+          "WARN: Couldn't find axis " + axisNumber + " in mapping! Perhaps device reported capabilities improperly!");
+      return;
+    }
+    m_axisStates[axisIndex] = value;
+  }
+
+  private void handleButton(short buttonNumber, boolean buttonState) {
+    int buttonIndex = m_buttonEventCodes.indexOf((int) buttonNumber);
+    if (buttonIndex < 0) {
+      System.err.println("WARN: Couldn't find button " + buttonNumber
+          + " in mapping! Perhaps device reported capabilities improperly!");
+      return;
+    }
+    /* Record as a button pressed if it was up and is now down */
+    m_buttonPressed[buttonIndex] = (!m_buttonStates[buttonIndex] && buttonState);
+    /* A button was released if it was down and is now up */
+    m_buttonReleased[buttonIndex] = (m_buttonStates[buttonIndex] && !buttonState);
+
+    m_buttonStates[buttonIndex] = buttonState;
   }
 
   /**
@@ -62,7 +191,7 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return The state of the button.
    */
   public boolean getRawButton(int button) {
-    return true;
+    return m_buttonStates[button - 1];
   }
 
   /**
@@ -78,7 +207,18 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return Whether the button was pressed since the last check.
    */
   public boolean getRawButtonPressed(int button) {
-    return true; // m_ds.getStickButtonPressed(m_port, (byte) button);
+    boolean retval;
+    if (m_buttonPressed[button - 1]) {
+      /*
+       * Clear the button pressed state since we're about to tell the robot code it
+       * happened
+       */
+      m_buttonPressed[button - 1] = false;
+      retval = true;
+    } else {
+      retval = false;
+    }
+    return retval;
   }
 
   /**
@@ -94,7 +234,18 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return Whether the button was released since the last check.
    */
   public boolean getRawButtonReleased(int button) {
-    return true; // m_ds.getStickButtonReleased(m_port, button);
+    boolean retval;
+    if (m_buttonReleased[button - 1]) {
+      /*
+       * Clear the button pressed state since we're about to tell the robot code it
+       * happened
+       */
+      m_buttonReleased[button - 1] = false;
+      retval = true;
+    } else {
+      retval = false;
+    }
+    return retval;
   }
 
   /**
@@ -104,7 +255,7 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return The value of the axis.
    */
   public double getRawAxis(int axis) {
-    return 100 + axis; // m_ds.getStickAxis(m_port, axis);
+    return m_axisStates[axis];
   }
 
   /**
@@ -118,7 +269,7 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return the angle of the POV in degrees, or -1 if the POV is not pressed.
    */
   public int getPOV(int pov) {
-    return 90; // m_ds.getStickPOV(m_port, pov);
+    return -1;
   }
 
   public int getPOV() {
@@ -131,21 +282,21 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return the number of axis for the current HID
    */
   public int getAxisCount() {
-    return 6; // m_ds.getStickAxisCount(m_port);
+    return m_axesCount;
   }
 
   /**
    * For the current HID, return the number of POVs.
    */
   public int getPOVCount() {
-    return 1; // m_ds.getStickPOVCount(m_port);
+    return 0;
   }
 
   /**
    * For the current HID, return the number of buttons.
    */
   public int getButtonCount() {
-    return 12; // m_ds.getStickButtonCount(m_port);
+    return m_buttonsCount;
   }
 
   /**
@@ -154,7 +305,7 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return the type of the HID.
    */
   public HIDType getType() {
-    return HIDType.of(1); // m_ds.getJoystickType(m_port)
+    return HIDType.kHIDGamepad;
   }
 
   /**
@@ -163,7 +314,7 @@ public abstract class RIOGenericHID extends GenericHID {
    * @return the name of the HID.
    */
   public String getName() {
-    return "HID Name Goes Here"; // m_ds.getJoystickName(m_port);
+    return m_device.getDeviceName();
   }
 
   /**
